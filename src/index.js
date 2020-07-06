36e3ae237aa12bd66b0a64e9830ba57cfd38bf9f
curtismn87 <curtismn87@yahoo.com>
2020-07-05 20:26:21 -0400

    Fixed typos

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const App = () => {
 	return (
 		<div className="ui container comments">
 			<div className="comment">
-					<a href="/" ckassName="avatar">
+					<a href="/" className="avatar">
 					<img alt="avatar" src={faker.image.avatar()} />
 					</a>
 				<div className="content">
 						<a href="/" className="author">
 							Sam
 						</a>
-					<div className="ametadata">
+					<div className="metadata">
 						<span className="date">Today at 6:00pm</span>
 					</div>
 					<div className="text">Nice Blog Post!</div>
